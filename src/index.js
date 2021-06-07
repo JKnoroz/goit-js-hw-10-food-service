6b8e72ecbb61bcf04e34c93d1829329ec5fb3a56
Yuliya <jknoroz@gmail.com>
2021-06-07 21:51:30 +0300

    start theme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitcher = document.querySelector('.theme-switch__toggle');
+themeSwitcher.addEventListener('change', changeTheme);
+function changeTheme() {
+  console.log('change theme');
+}
+
+function makeLightTheme() {
+  body.classList.add('light-theme');
+}
+
+function makeDarkTheme() {
+  body.classList.add('dark-theme');
+}
